5243a019bfa27466a408f8dfc453b1a59e910b38
Smarangk13 <smarangk@gmail.com>
2016-11-14 19:55:19 -0800

    reading and filtering useful info from convo file

---
+++ filefilter.py
@@ -0,0 +1,49 @@
+def compare(list,str):
+	r=0
+	for i in list:
+		if(i.find(str)>=0):
+			r=i
+	return r
+
+# Define a filename.
+filename = "Convo1.txt"
+ 
+# Open the file as f.
+\# The function readlines() reads the file.
+with open(filename) as f:
+	content = f.read().splitlines()
+
+# make an array which will allow us to know which answers correspond to which questions for quick access whhen compared to reading the file multiple times
+# first col=qnums following cols point to other questions corresponding to answer selected
+# third index is relationship points gained
+
+#step 1 find participant names in chat
+# stap 2 
+special=["block","A","min","max"]
+participants=[]
+convo=[][]
+i=0 
+in_block=0
+in_ans=0
+
+# Show the file contents line by line.
+for line in content:
+	if(participants==[]):
+		a=line.find("Participants:")
+		if (a>=0):
+			t=line[line.find(":")+1:]
+			participants=t.split()
+	
+	if(line.find("//")>=0):
+		if(
+
+	else:
+		convo.append(i)
+		i+=1	
+		convo[i-1].append
+
+	# print(line)
+
+	
+
+print(participants)
