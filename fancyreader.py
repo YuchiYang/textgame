197b05892feeccc861e539f4ce1f37a88b0a62ae
Smarangk13 <smarangk@gmail.com>
2016-11-20 01:06:41 -0800

    reading very efficiently

--- fancyreader.py
+++ fancyreader.py
@@ -1,6 +1,32 @@
 import json
 from pprint import pprint
 
-with open('convo1.json') as data_file:
+with open('.\\files\\convo1.json') as data_file:
     data = json.load(data_file)
-pprint(data)
+# pprint(data)
+
+totalblocks=data['Blocks']
+Block="Block"
+
+# for i in range (totalblocks):
+#     Cb=Block+str(i+1)
+#
+#     Cc=data[Cb]['convo']+".txt"
+#     # print(Cc,Cb)
+#
+#     with open (Cc) as f:
+#         content=f.read().splitlines()
+#     for line in content:
+#         print (line)
+
+Cb=Block+"1"
+
+Cc=data[Cb]['convo']+".txt"
+# print(Cc,Cb)
+
+with open (Cc) as f:
+    content=f.read().splitlines()
+for line in content:
+    print (line)
+for i in range(data[Cb]['answers']):
+    print(i+1,".",data[Cb]['responses'][i]['text'])
